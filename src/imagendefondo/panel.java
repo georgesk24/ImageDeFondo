a08447dabddc11633ea82774eda5514d13222be2
georgesk24 <jorgepotes1994@gmail.com>
2020-09-20 15:45:45 -0500

    update

--- src/imagendefondo/panel.java
+++ src/imagendefondo/panel.java
@@ -13,11 +13,24 @@ public class panel extends JPanel{
     
     @Override
     public void paint(Graphics g){
-        Dimension dimension = this.getSize();
-        ImageIcon icon = new ImageIcon(getClass().getResource("/images/back.jpg"));
+        Dimension dimension = this.getSize();        
+        ImageIcon icon = new ImageIcon(getClass().getResource("/images/background_286_176.jpg"));
         g.drawImage(icon.getImage(), 0, 0, dimension.width, dimension.height, null);
         setOpaque(false);
         super.paintChildren(g);
-    }    
+    }   
+    
+    /*
+    @Override
+    public void paint(Graphics g){
+        
+        Dimension dimension = this.getSize();        
+        URL url = this.getClass().getResource("/images/tenor.gif");
+        Image image = new ImageIcon(url).getImage();
+        g.drawImage(image, 0, 0, dimension.width, dimension.height, this);
+        setOpaque(false);
+        super.paintChildren(g);
+    
+    }*/    
     
 }
